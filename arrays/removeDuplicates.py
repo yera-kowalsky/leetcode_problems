ac0a8cb59a838899bd54afc0c960b4bef2581904
Yerkebulan <yerkebulan014@gmail.com>
2025-01-06 12:46:12 +0100

    removeDuplicates

---
+++ arrays/removeDuplicates.py
@@ -0,0 +1,13 @@
+nums = [1, 1, 2, 2, 3]
+
+
+def removeDuplicates(nums):
+    i = 1
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i - 1]:
+            nums[i] = nums[j]
+            i += 1
+    return i
+
+print(removeDuplicates(nums))
+# output 3
