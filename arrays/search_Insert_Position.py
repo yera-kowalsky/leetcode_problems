0201fca80e6e2d1ae3355f3470e5f8996b92de82
Yerkebulan <yerkebulan014@gmail.com>
2025-01-07 12:01:49 +0100

    searchInsert

---
+++ arrays/search_Insert_Position.py
@@ -0,0 +1,19 @@
+n = [1, 3, 5, 6]
+t = 4
+
+
+def searchInsert(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+
+print(searchInsert(n, t))
