b3d5559c71008be250f7093157aa4dc98a0a8a56
Yerkebulan <yerkebulan014@gmail.com>
2025-01-08 14:20:42 +0100

    plusOne

--- arrays/plus_one.py
+++ arrays/plus_one.py
@@ -1,7 +1,14 @@
-digits_1 = [1,2,3]
+digits_1 = [1, 2, 3]
+
 
 def plusOne(digits):
-
+    for i in range(len(digits_1) - 1, -1, -1):
+        if digits[i] + 1 != 10:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+        if i == 0:
+            return [1] + digits
 
 
 print(plusOne(digits_1))
